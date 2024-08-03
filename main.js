515209fddc590ac4d90b31242801495c68738839
rafaseoanes <rafaseoanes9@gmail.com>
2024-08-02 19:38:08 -0500

    Cards to show information implemented

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import { getData } from "./getData.js";
 
-
 // document.querySelector('#app').innerHTML = `
 
 // `
@@ -11,6 +10,10 @@ const submitBtn = document.getElementById("submitBtn");
 const locationInput = document.getElementById("locationInput");
 const temp = document.getElementById("temp");
 const locationName = document.getElementById("locationName");
+const timezone = document.getElementById("timezone");
+
+const windInfo = document.getElementById("windInfo");
+const humidityInfo = document.getElementById("humidityInfo");
 
 submitBtn.addEventListener("click", (event) => {
   const userInput = locationInput.value;
@@ -20,10 +23,19 @@ submitBtn.addEventListener("click", (event) => {
 
 async function parseData(city = "londres") {
   const data = await getData(city);
-  
-  temp.innerText = data.currentConditions.temp;
+
+  console.log(data);
+  temp.innerText = `${data.currentConditions.temp} °F`;
   let cityName = data.resolvedAddress.split(",", 1);
   locationName.innerText = cityName;
+
+  let date = new Date();
+  let options = { timeZone: `${data.timezone}` };
+  let currentDate = date.toLocaleString("en-US", options);
+  timezone.innerText = currentDate;
+
+  windInfo.innerText = `${data.currentConditions.windspeed} km/h`;
+  humidityInfo.innerText = `${data.currentConditions.humidity} %`;
 }
 
 parseData();
